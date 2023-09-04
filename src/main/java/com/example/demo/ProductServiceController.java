688c29dc0e9920200fa22e6c4c72cb896d9366d3
omer358 <omermaki358@gmail.com>
2023-09-04 15:50:18 +0200

    GET/POST requests

---
+++ src/main/java/com/example/demo/ProductServiceController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ProductServiceController {
+    private static Map<String,Product> productRepo = new HashMap<>();
+    static {
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Honey");
+        productRepo.put(honey.getId(), honey);
+
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProduct(){
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products  ",method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product){
+        productRepo.put(product.getId(),product);
+        return new ResponseEntity<>("Product is created successfully",HttpStatus.CREATED);
+    }
+}
